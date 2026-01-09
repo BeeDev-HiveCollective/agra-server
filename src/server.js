f0c319e68722ec74ea21791b9ed4ac98f5af6271
Christopher J <chrisbjones1987@yahoo.com>
2026-01-09 12:16:50 -0600

    Fix - server port conflict - change from 5000 to 3001

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
     res.json({ message: "AG Reese Server Running" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
