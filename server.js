b72ec5fe408c23e5f52553a1e916ad4b59359a93
melissa-24 <mlongenberger@codingdojo.com>
2026-06-05 11:30:39 -0400

    adding files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config()
 const express = require('express')
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const app = express()
-require("dotenv").config()
+
 
 app.use(cors({
     credentials: true,
